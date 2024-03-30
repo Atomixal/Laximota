e914c9093a3cd702cac36f5f73766096a1e785f0
Atomixal <71277875+Atomixal@users.noreply.github.com>
2024-03-30 12:23:36 +0000

    Fixed the maths questions so now they can recive a response from the user.

--- main.js
+++ main.js
@@ -1,57 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const { TOKEN } = require('./config.json')
-
-/* IDEAS FOR THE BOT:
-
-1) One word story, and when someone breaks the chain the story time ends and the bot compiles all the words that have been sent into a story in one message.
-
-2) Maths questions with a leaderboard to get more people doing maths.
-
-3) XP system because everyone has a fucking XP system.
-
-4) A welcome message for anyone joining the server
-
-5) A voting system, people can vote between 2 - 9 things and it keeps track of who's voted using reactions.
-
-6) Credits :D
-
-7) Music playing because everything has a fucking music player.
-
-*/
-
-class MathsQuestionGenerator {
-    static amount = 0;
-    static answers = 0;
-    static questions = " ";
-    constructor(inAmount) {
-        MathsQuestionGenerator.amount = inAmount;
-    }
-
-    // Method to output maths questions
-    static generateMathsQuestions() {
-        for (let i = 0; i < MathsQuestionGenerator.amount; i++) {
-
-            NextMathQuestion = Math.random() * (4 - 1) + 1;
-            if (NextMathQuestion === 1) {
-                this.MakeAdditionQuestions();
-            }
-        }
-    }
-
-
-    static randomNumberGenerator() {
-        return Math.random() * (100 - 1) + 1;
-    }
-
-    static MakeAdditionQuestions() {
-        for (let i = 0; i < MathsQuestionGenerator.amount; i++) {
-            let randomNum1 = this.randomNumberGenerator();
-            let randomNum2 = this.randomNumberGenerator();
-            this.questions.push(`${randomNum1} + ${randomNum2}`);
-            this.answers.push(randomNum1 + randomNum2);
-        }
-    }
-}
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
 const client = new Client({
     intents: [
@@ -61,36 +10,20 @@ const client = new Client({
     ]
 });
 
-
 client.on("ready", async () => {
-    console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}`);
 
-    // Register slash commands
-    const commands = await client.application.commands.set([
-        {
-            name: 'ping',
-            description: 'Returns the latency of the bot and the Discord API.'
-        },
-        {
-            name: 'math_question',
-            description: 'Returns a maths question.'
-        }
-    ]);
-    console.log(`${commands.size} commands registered.`);
+    const ping = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Returns the latency of the bot and the Discord API.');
+    
+    const credits = new SlashCommandBuilder()
+        .setName('credits')
+        .setDescription('Returns the credits for everyone who contributed to it.');
+    const math_question = new SlashCommandBuilder()
+        .setName('math_question')
+        .setDescription('Returns a math question.');
 });
-
-function RandomNumberGeneration() {
-    return Math.round(Math.random() * (1, 100) + 1);
-}
-
-function RandomAdditionQuestion() {
-    let rand1 = RandomNumberGeneration();
-    let rand2 = RandomNumberGeneration();
-    answers = rand1 + rand2
-
-    return (`What is ${rand1} + ${rand2}?`);
-}
-
 
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
@@ -98,21 +31,37 @@ client.on('interactionCreate', async (interaction) => {
     const { commandName } = interaction;
 
     if (commandName === 'ping') {
-        interaction.reply(`Pong! Latency is ${Math.abs(Date.now() - interaction.createdTimestamp)}ms, and API latency is ${Math.round(client.ws.ping)}ms.`);
+        interaction.reply(`Pong! Latency is ${Date.now() - interaction.createdTimestamp}ms, and API latency is ${Math.round(client.ws.ping)}ms.`);
     }
 
     if (commandName === 'math_question') {
-        interaction.reply(`${RandomAdditionQuestion()}`);
-        client.on('message', async (response) => {
-            const { ans } = response
-            if (ans === answers) {
-                response.reply(`Congratulations ${response.user.tag}! You're correct.`)
-            }
-            else {
-                response.reply(`Sorry, ${response.user.tag}. You are incorrect. The correct answer was ${answers}.`)
-            }
-        });
+        let num1 = Math.floor(Math.random() * 100) + 1;
+        let num2 = Math.floor(Math.random() * 100) + 1;
+    
+        let correctAnswer = num1 + num2;
+    
+        interaction.reply(`What is ${num1} + ${num2}?`)
+            .then(() => {
+                const collectorFilter = response => {
+                    return !response.author.bot && parseInt(response.content) === correctAnswer; // Checks if the response is NOT from the bot and equal to the correct answer
+                };
+    
+                // Await a single response from the user
+                interaction.channel.awaitMessages({ filter: collectorFilter, max: 1, time: 30000, errors: ['time'] })
+                    .then(collected => {
+                        interaction.followUp(`${collected.first().author} got the correct answer!`);
+                    })
+                    .catch(() => {
+                        interaction.followUp(`Looks like nobody got the answer this time. The correct answer was ${correctAnswer}`);
+                    });
+            });
+    }
+    
+
+    if (commandName === 'credits') { 
+        interaction.reply('Made by <@584844292137418763> using Discord.js. Thank you for using Laximota <3');
     }
 });
-
+ 
 client.login(TOKEN);
+ 
