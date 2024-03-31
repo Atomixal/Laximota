7b003b75c3e9bf8dff48dac65432ea856ad2e858
Atomixal <71277875+Atomixal@users.noreply.github.com>
2024-03-31 22:58:00 +0100

    Updated to Discord.js v14.14.1

--- main.js
+++ main.js
@@ -1,43 +1,42 @@
-const { Client, Intents } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { TOKEN } = require('./config.json');
-const { SlashCommandBuilder, messageLink } = require('@discordjs/builders');
+const { SlashCommandBuilder } = require('@discordjs/builders');
 const { QuickDB } = require('quick.db');
 
 const db = new QuickDB();
-
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILD_MEMBERS
-    ]
-});
-
-
-// Slash commands
-client.on("ready", async () => {
-    console.log(`Logged in as ${client.user.tag}`);
-
-    const ping = new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Returns the latency of the bot and the Discord API.');
-    
-    const credits = new SlashCommandBuilder()
-        .setName('credits')
-        .setDescription('Returns the credits for everyone who contributed to it.');
-    const math_question = new SlashCommandBuilder()
-        .setName('math_question')
-        .setDescription('Returns a math question.');
-    const message_count = new SlashCommandBuilder()
-        .setName('message_count')
-        .setDescription('Outputs a list of who has the most messages sent in the server,');
-});
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
 const scores = {};
 
+client.on("ready", () => {
+    console.log(`Logged in as ${client.user.tag}`);
+    
+    const pingCommand = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Returns the latency of the bot and the Discord API.');
+
+    const creditsCommand = new SlashCommandBuilder()
+        .setName('credits')
+        .setDescription('Returns the credits for everyone who contributed to it.');
+
+    const mathQuestionCommand = new SlashCommandBuilder()
+        .setName('math_question')
+        .setDescription('Returns a math question.');
+
+    const messageCountCommand = new SlashCommandBuilder()
+        .setName('message_count')
+        .setDescription('Outputs a list of who has the most messages sent in the server.');
+    
+    const commands = [pingCommand, creditsCommand, mathQuestionCommand, messageCountCommand];
+    
+    client.guilds.cache.forEach(guild => {
+        guild.commands.set(commands);
+    });
+});
+
 client.on('interactionCreate', async (interaction) => {
     if (!interaction.isCommand()) return;
-
+    
     const { commandName } = interaction;
 
     if (commandName === 'ping') {
@@ -59,33 +58,33 @@ client.on('interactionCreate', async (interaction) => {
                 // Await a single response from the user
                 interaction.channel.awaitMessages({ filter: collectorFilter, max: 1, time: 30000, errors: ['time'] })
                     .then(collected => {
-                        interaction.followUp(`${collected.first().author} got the correct answer!`);
                         scores[collected.first().author.id] = (scores[collected.first().author.id] || 0) + 1;
 
                         let scoresArray = Object.entries(scores);
                         
                         scoresArray.sort((a, b) => b[1] - a[1]);
 
-                        let [userId, userScore] = scoresArray[0];
-
-                        interaction.channel.send(`<@${userId}> is in the lead with ${userScore} point(s)!`);    
+                        let [userId, userScore] = scoresArray[0];  
+                        interaction.channel.send(`${collected.first().author} got the correct answer!\n`);
+                        interaction.channel.send(`<@${userId}> is in the lead with ${userScore} point(s)!`);  
                     })
                     .catch(() => {
                         interaction.followUp(`Looks like nobody got the answer this time. The correct answer was ${correctAnswer}`);
                     }); 
             });
     }
-    
+
     if (commandName === 'credits') { 
         interaction.reply('Made by <@584844292137418763> using Discord.js. Thank you for using Laximota <3.');
     }
+
+    if (commandName === 'message_count') {
+        // Message count logic
+    }
 });
 
+client.on("messageCreate", async (message) => {
+    // Message counting and XP system logic
+});
 
-client.on("messageCreate", async(message) => { // Message counting system, will update to have some sort of xp system.
-    const authorId = message.author.id;
-    db.add(`messageCount_${message.author.id}`, 1);
-})
- 
 client.login(TOKEN);
-    
